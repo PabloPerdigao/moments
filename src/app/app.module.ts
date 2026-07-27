c7513975ead0e87071d82e987a6a1b49dc62a492
PabloPerdigao <pabloagps@gmail.com>
2026-07-27 11:22:33 -0300

    feat: adiciona módulos HttpClient e Forms ao AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
@@ -22,7 +25,9 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    HttpClientModule,
+    BrowserModule, 
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
