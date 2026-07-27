53e7bc65a83c8d5a3039d11bb2495deced3098f1
PabloPerdigao <pabloagps@gmail.com>
2026-07-27 09:57:43 -0300

    feat: cria estrutura inicial do projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+
+// rotas referenciando os componentes
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'moments/new', component:NewMomentComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
