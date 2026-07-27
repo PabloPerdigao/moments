47ec9253023c3668b257cb5f9a4fb8dd6f6491a7
PabloPerdigao <pabloagps@gmail.com>
2026-07-27 12:05:12 -0300

    feat: implementa validação de formulário e exibe mensagens de erro

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
@@ -9,9 +10,34 @@ export class MomentFormComponent implements OnInit {
 
   @Input() btnText!: string;
 
+  momentForm !: FormGroup;
+
   constructor() { }
 
-  ngOnInit(): void {
+  // inicializa o formulário com os campos necessários
+  ngOnInit(): void { 
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  // retorna o valor do campo title do formulário
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+  // retorna o valor do campo description do formulário
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid) {
+      return;
+    }
+    console.log('submit');
   }
 
 }
