6db032c6b7cfd732f76c56ee13bc62ee3a28f28d
PabloPerdigao <pabloagps@gmail.com>
2026-07-29 08:21:27 -0300

    feat: implementa lógica de exibição e pesquisa de momentos na página inicial

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MomentService } from '../../../services/moment.service';
+
+import { Moment } from '../../../Moment';
+
+import { environment } from '../../../../environments/environment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = []; // pegar todos os momentos do backend
+  moments: Moment[] = []; // moments filtrados
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+  faSearch = faSearch; // ícone de pesquisa
+  searchTerm: string = ''; // termo de pesquisa
+
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
+    // Chamada do serviço para buscar todos os momentos do backend
+    this.momentService.getMomments().subscribe((items) => {
+      const data = items.data;
+
+      // Formata a data de criação de cada momento para o formato local 'pt-BR'
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR',
+        );
+      });
+
+      // Atribui os momentos formatados às variáveis
+      this.allMoments = data;
+      this.moments = data;
+    });
   }
 
+  // Função de pesquisa 
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement // Converte o evento para um elemento de entrada HTML
+    const value = target.value; // Obtém o valor do campo de entrada
+
+    // Filtra os momentos com base no termo de pesquisa, ignorando maiúsculas e minúsculas
+    this.moments = this.allMoments.filter(moment => {
+      return moment.title.toLowerCase().includes(value);
+    });
+  } 
+
+  
 }
