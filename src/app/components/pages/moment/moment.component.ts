8c6409c4ff66859950ad7b9fa64a2380a9d9ca96
PabloPerdigao <pabloagps@gmail.com>
2026-07-30 08:20:19 -0300

    feat: adiciona funcionalidade de comentários com validação e integração ao serviço de comentários

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+} from '@angular/forms';
+
 import { MomentService } from '../../../services/moment.service';
 import { MessagesService } from '../../../services/messages.service';
+import { CommentService } from '../../../services/comment.service';
+
 import { Moment } from '../../../Moment';
 
+import { Comment } from '../../../Comment';
 
 import { environment } from 'src/environments/environment';
 
-import { faTimes, faEdit} from '@fortawesome/free-solid-svg-icons';
-
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -19,14 +29,17 @@ export class MomentComponent implements OnInit {
   moment?: Moment;
   baseApiUrl = environment.baseApiUrl; // URL base da API definida no arquivo environment.ts
 
-  faEdit = faEdit;
   faTimes = faTimes;
+  faEdit = faEdit;
+
+  commentForm!: FormGroup;
 
   constructor(
     private momentService: MomentService,
     private route: ActivatedRoute,
     private messagesService: MessagesService,
-    private router: Router
+    private router: Router,
+    private commentService: CommentService
   ) {}
 
   ngOnInit(): void {
@@ -36,14 +49,52 @@ export class MomentComponent implements OnInit {
     this.momentService
       .getMoment(id)
       .subscribe((item) => (this.moment = item.data));
+
+    // Inicializa o formulário de comentários com validação para os campos 'text' e 'username'
+    this.commentForm = new FormGroup({
+      text: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required])
+    });
+  }
+
+  // Método para enviar um comentário
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  // Método para enviar um comentário
+  get username() {
+    return this.commentForm.get('username')!;
   }
 
   // Método para remover um momento específico pelo ID
-  async removeHandler(id:number) {
+  async removeHandler(id: number) {
     // Chama o serviço para remover o momento pelo ID e exibe uma mensagem de sucesso
     await this.momentService.removeMoment(id).subscribe();
-    this.messagesService.add("Momento excluído com sucesso!");
+    this.messagesService.add('Momento excluído com sucesso!');
 
     this.router.navigate(['/']);
   }
+
+  // Método para enviar um comentário
+  async onSubmit(formDirective: FormGroupDirective) {
+    if (this.commentForm.invalid) {
+      return;
+    }
+
+    const data: Comment = this.commentForm.value; // Obtém os dados do formulário de comentário
+
+    data.moment_id = Number(this.moment!.id);
+
+    // Chama o serviço para criar um novo comentário e adiciona o comentário à lista de comentários do momento
+    await this.commentService
+      .createComment(data)
+      .subscribe((comment) => this.moment!.comments!.push(comment.data));
+    
+        this.messagesService.add('Comentário adicionado!')
+
+        this.commentForm.reset();
+
+        formDirective.resetForm();
+    }
 }
--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import {Comment} from '../Comment';
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  // URL base da API definida no arquivo environment.ts
+  private baseApiUrl = environment.baseApiUrl
+  private apiUrl = `${this.baseApiUrl}api/moments`;
+
+
+  constructor(private http: HttpClient) {}
+  
+  // Método para criar um novo comentário, recebendo os dados do comentário em um objeto Comment
+  createComment(data: Comment): Observable<Response<Comment>> {
+    const url = `${this.apiUrl}/${data.moment_id}/comments`; // URL completa da API para criar comentários
+    return this.http.post<Response<Comment>>(url, data);
+  }
 }
